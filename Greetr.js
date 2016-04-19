7694cca54de2a99a4e58b67e3e01dca3ac8b4acc
Justin <justin.vanvoorst@gmail.com>
2016-04-18 19:59:14 -0600

    added comments

--- Greetr.js
+++ Greetr.js
@@ -1,9 +1,11 @@
 (function(global, $) {
 
+	// this allow to use greetr without needing to use the new keyword in app.js, creates a new object
 	var Greetr = function(fistName, lastName, language) {
 		return new Greetr.init(fistName,lastName, language)
 	}
 
+	//there variable will be hidden in the scope of this library and never accessible outside it because the whole library is an IIFE
 	var supportedLangs = ['en', 'es'];
 	var greetings = {
 		en: 'Hello',
@@ -18,12 +20,12 @@
 		es: 'Incicio sesion'
 	};
 
+	// add methods to the prototype to save memory instead of adding to each created object
 	Greetr.prototype = {
-
+		// this will refer to the calling object at execution time
 		fullName: function() {
 			return this.firstName + ' ' + this.lastName;
 		},
-
 		validate: function() {
 			if (supportedLangs.indexOf(this.language) === -1) {
 				throw "Invalid language";
@@ -32,11 +34,13 @@
 		greeting: function() {
 			return greetings[this.language] + ' ' + this.firstName + '!';
 		},
+		// retrieve messages from greetings object by using [] syntax to refer to properties
 		formalGreeting: function() {
 			return formalGreetings[this.language] + ' ' + this.fullName();
 		},
 		greet: function(formal) {
 			var msg;
+			// if undefined or null will be coerced to false
 			if (formal) {
 				msg = this.formalGreeting();
 			}
@@ -46,6 +50,8 @@
 			if (console) {
 				console.log(msg);
 			}
+			// this refers to calling object at execution time, makes the method chainable
+			// ex - object.greet(true).log
 			return this;
 		},
 		log: function() {
@@ -79,6 +85,7 @@
 
 	};
 
+	// object is created here
 	Greetr.init = function(firstName, lastName, language) {
 
 		var self = this;
@@ -89,8 +96,10 @@
 
 	}
 
+	// trick borrowed from jQuery so we don't have to use the new keyword
 	Greetr.init.prototype = Greetr.prototype;
 
+	// attach Greetr and the shorthand G$ to the glodal object
 	global.Greetr = global.G$ = Greetr
 
 }(window, jQuery));
